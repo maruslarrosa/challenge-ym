d597db923ea386f28846197d280e2d35d27cd899
maruslarrosa <larrosa.marilina@gmail.com>
2023-04-13 16:13:11 -0300

    Adding Withdrawal functionality and its assets

--- src/pages/Withdraw.page.tsx
+++ src/pages/Withdraw.page.tsx
@@ -1,3 +1,60 @@
+import uploadFile from '../assets/upload-file.svg';
+import addAccount from '../assets/add-account.svg';
+import { useRef, useState } from 'react';
+import { useAppSelector } from '../redux/hooks';
+import { accountType } from '../types/user.types';
+
 export const WithdrawPageComponent = (): JSX.Element => {
-  return <p>Withdraw</p>;
+  const inputRef = useRef<HTMLInputElement>(null);
+  const linkedAccounts: accountType[] = useAppSelector(
+    (state) => state.user.linkedAccounts
+  );
+  const [filter, setFilter] = useState<string>('');
+
+  const handleFilterChange = () => {
+    if (inputRef.current) {
+      setFilter(inputRef.current?.value);
+    }
+  };
+
+  const renderFilteredAccounts = () => {
+    const filteredAccounts = linkedAccounts.filter((account) =>
+      account.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    return filteredAccounts.map((acc) => {
+      return (
+        <>
+          <p key={acc.email}>{acc.name}</p>
+          <p>{acc.email}</p>
+        </>
+      );
+    });
+  };
+
+  return (
+    <>
+      <p>Nuevas cuentas</p>
+      <img src={addAccount} alt='Add Account icon' />
+      <p>Agregar Cuenta</p>
+      <img src={uploadFile} alt='Upload File icon' />
+      <p>Subir Archivo</p>
+      <input
+        type='text'
+        placeholder='Buscar por nombre o email'
+        ref={inputRef}
+        onChange={handleFilterChange}
+      ></input>
+      <p>Tus cuentas</p>
+      {!filter
+        ? linkedAccounts.map((acc) => {
+            return (
+              <>
+                <p key={acc.email}>{acc.name}</p>
+                <p>{acc.email}</p>
+              </>
+            );
+          })
+        : renderFilteredAccounts()}
+    </>
+  );
 };
